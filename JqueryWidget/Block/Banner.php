218c688243418a6dc2374d4a15c517cdb6de6e5b
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-07-06 01:36:04 +0530

    jquery widget custom image slider

---
+++ JqueryWidget/Block/Banner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Excellence\JqueryWidget\Block;
+
+class Banner extends \Magento\Framework\View\Element\Template
+{
+    protected $_values = null;
+    protected $_scopeConfig;
+    protected $helperData;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Excellence\JqueryWidget\Helper\Data $helperData,
+
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+    public function getBannerConfig()
+    {
+        return $this->helperData->getBannerConfig();
+    }
+}
